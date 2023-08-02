fd2bf2c5524232e66d738c1a11f7d6f64e075732
P.Ivo <pivolluz@gmail.com>
2023-08-02 17:47:07 -0300

    implmentação didatica de Deep Etags

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -75,13 +75,31 @@ public class FormaPagamentoController {
 	
 	@GetMapping(value = "/{formaPagamentoId}")
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<FormaPagamentoModel> buscar (@PathVariable Long formaPagamentoId) {
+	public ResponseEntity<FormaPagamentoModel> buscar (@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+	    
+	    String eTag = "0";
+	    
+	    OffsetDateTime dataAtualizacao = repository
+	            .getDataAtualizacaoById(formaPagamentoId);
+	    
+	    if (dataAtualizacao != null) {
+	        eTag = String.valueOf(dataAtualizacao.toEpochSecond());
+	    }
+	    
+	    if (request.checkNotModified(eTag)) {
+	        return null;
+	    }
+		
 		
 		FormaPagamento formaPagamento = service.buscarOuFalhar(formaPagamentoId);
 		
 		FormaPagamentoModel formaPagamentoModel = assembler.toModel(formaPagamento);
 		
-		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(10,TimeUnit.SECONDS)).body(formaPagamentoModel);
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(10,TimeUnit.SECONDS))
+				.eTag(eTag)
+				.body(formaPagamentoModel);
 	}
 	
 	
